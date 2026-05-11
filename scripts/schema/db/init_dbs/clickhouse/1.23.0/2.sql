ea913329c494f28223911fe93a5cfc5d9073c94a
Sathvik Shetty <74665830+sathvik-shettyy@users.noreply.github.com>
2026-05-11 11:27:28 +0530

    lightweight railway demo

--- scripts/schema/db/init_dbs/clickhouse/1.23.0/2.sql
+++
@@ -1,5 +0,0 @@
-SELECT throwIf((SELECT openreplay_migration_state()) != 1, 'Previous step is not done') AS check;
-
-ALTER TABLE experimental.sessions
-    MODIFY COLUMN platform Enum8('web'=1,'mobile'=2) DEFAULT 'web';
-CREATE OR REPLACE FUNCTION openreplay_migration_state AS() -> 2;
